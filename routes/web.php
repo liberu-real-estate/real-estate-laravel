f0e25e6dcdce2c98a42c78455320522dff4866c6
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-13 03:29:28 +0000

    Remove migrations

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::post('/payments/session', [\App\Http\Controllers\PaymentController::class
 Route::get('/payments/success', [\App\Http\Controllers\PaymentController::class, 'handlePaymentSuccess']);
 Route::get('/booking-calendar', [\App\Http\Livewire\BookingCalendar::class])->middleware('auth')->name('booking.calendar');
 
+require __DIR__.'/socialstream.php';
