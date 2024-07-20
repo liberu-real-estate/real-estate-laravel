7c94fbc7799270c4e214651c7f27ed51012feeee
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-20 15:36:28 +0000

    Fix Property Model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -1,4 +1,10 @@
 <?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Property extends Model
 {
     protected $fillable = [
