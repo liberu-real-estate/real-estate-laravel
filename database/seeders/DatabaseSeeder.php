9bc8d251449390aa1c04e04725fb528cc469bc93
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-20 18:09:09 +0000

    Upgrade composer and npm

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             PropertySeeder::class,
+            PermissionsSeeder::class,
+            RolesSeeder::class,
             // Add other seeders here if needed
         ]);
     }
