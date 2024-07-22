7e0dfb00d8043cc685c63c5a5512bc63a5b96b2b
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-22 00:06:11 +0000

    Update

--- app/Providers/TeamServiceProvider.php
+++ app/Providers/TeamServiceProvider.php
@@ -22,17 +22,6 @@ class TeamServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Utils\BatchData::class, BatchData::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\Family::class, Family::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\FamilyEvent::class, FamilyEvent::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\FamilySlgs::class, FamilySlgs::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\Person::class, Person::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\PersonAsso::class, PersonAsso::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\PersonAlia::class, PersonAlia::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\PersonEvent::class, PersonEvent::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\Addr::class, Addr::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\Chan::class, Chan::class);
-        $this->app->bind(\FamilyTree365\LaravelGedcom\Models\Subm::class, Subm::class);
     }
 
     /**
