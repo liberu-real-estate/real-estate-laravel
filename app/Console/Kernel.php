f370f829b81ffa14b99d31df9876c763a81fd9b6
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-22 01:35:10 +0000

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Services\RightMoveService;
+use App\Services\OnTheMarketService;
 
 class Kernel extends ConsoleKernel
 {
