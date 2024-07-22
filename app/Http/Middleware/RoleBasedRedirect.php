1e4edab64263d6c7ad27e04892055a21047200ee
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-22 00:04:26 +0000

    Fixes

--- app/Http/Middleware/RoleBasedRedirect.php
+++ app/Http/Middleware/RoleBasedRedirect.php
@@ -22,30 +22,10 @@ class RoleBasedRedirect
     {
         if (Auth::check()) {
             $user = Auth::user();
-<<<<<<< HEAD
-            if ($user->hasRole('admin')) {
-                return redirect('/admin');
-            } elseif ($user->hasRole('staff')) {
-                return redirect('/staff');
-            } elseif ($user->hasRole('buyer')) {
-                return redirect('/buyer');
-            } elseif ($user->hasRole('seller')) {
-                return redirect('/seller');
-            } elseif ($user->hasRole('tenant')) {
-                return redirect('/tenant');
-            } elseif ($user->hasRole('landlord')) {
-                return redirect('/landlord');
-            } elseif ($user->hasRole('contractor')) {
-                return redirect('/contractor');
-            } elseif ($user->hasRole('free') || !$user) {
-               return $next($request);
-=======
             foreach ($this->roleRedirects as $role => $redirect) {
                 if ($user->hasRole($role)) {
                     return redirect($redirect);
-                }
->>>>>>> refs/remotes/origin/main
-            }
+                }            }
             // If user has 'free' role or no specific role, allow them to access /app
                return $next($request);
 
