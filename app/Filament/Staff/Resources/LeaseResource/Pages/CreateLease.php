a038b22cc430765930fd45d3d4d55637e2b4ea55
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-23 18:31:03 +0000

    Bug fix

--- app/Filament/Staff/Resources/LeaseResource/Pages/CreateLease.php
+++ app/Filament/Staff/Resources/LeaseResource/Pages/CreateLease.php
@@ -1,10 +1,5 @@
-use App\Filament\Staff\Resources\LeaseResource;
-use Filament\Resources\Pages\CreateRecord;
-
-class CreateLease extends CreateRecord
-{
-    protected static string $resource = LeaseResource::class;
-}
+<?php
+namespace App\Filament\Staff\Resources\LeaseResource\Pages;
 
 use App\Filament\Staff\Resources\LeaseResource;
 use Filament\Resources\Pages\CreateRecord;
