6fdd34a42ca784db248bd735ddd33bd14ba9666f
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-28 15:04:25 +0000

    Remove un-needed JS

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -15,7 +15,3 @@
         </div>
     </div>
 </nav>
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
