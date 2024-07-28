7196bc37db867ecd7e3789849d036f09f59e3a4a
Curtis Delicata <curtis.delicata@outlook.com>
2024-07-28 14:11:44 +0000

    Fixes

--- app/Filament/Resources/BookingResource.php
+++ app/Filament/Resources/BookingResource.php
@@ -5,9 +5,9 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\BookingResource\Pages;
 use App\Models\Booking;
 use Filament\Forms;
-use Filament\Resources\Form;
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Resources\Table;
+use Filament\Tables\Table;
 use Filament\Tables;
 
 class BookingResource extends Resource
