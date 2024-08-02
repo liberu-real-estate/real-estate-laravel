d298e39a803e1c65387e5b164efcb108a099283b
Joshua Kisb <joshuakisb@gmail.com>
2024-08-02 15:09:36 +0300

    fix property templates

--- app/Models/PropertyTemplate.php
+++ app/Models/PropertyTemplate.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PropertyTemplate extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'content'];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
