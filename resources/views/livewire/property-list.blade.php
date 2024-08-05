dad558f51f7fd1ec3d2615875ec6f230926736cb
raza9798 <m.raza9798@gmail.com>
2024-08-06 03:24:39 +0530

    search placeholder updated

--- resources/views/livewire/property-list.blade.php
+++ resources/views/livewire/property-list.blade.php
@@ -46,7 +46,7 @@
                             </div>
                             <input type="text" id="simple-search"
                                 class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="Search" required="">
+                                placeholder="Looking for a house, apartment, or commercial space? ..." required="">
                         </div>
 
                         <button type="button"
