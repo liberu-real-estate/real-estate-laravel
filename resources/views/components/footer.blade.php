8534b370b6c9ecec5df6a8fc77ec6179ec038a75
raza9798 <m.raza9798@gmail.com>
2024-08-06 03:59:34 +0530

    code review and refectoring

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,3 @@
-{{-- <footer class="bg-green-900 text-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div class="flex flex-col md:flex-row justify-between items-center">
-            <div class="mb-4 md:mb-0">
-                <a href="/" class="text-lg font-semibold">{{ \App\Helpers\SiteSettingsHelper::get('name') }}</a>
-            </div>
-            <nav class="mb-4 md:mb-0">
-                <ul class="flex flex-wrap justify-center md:justify-end space-x-4">
-                    <li><a href="/about" class="hover:text-gray-300">About Us</a></li>
-                    <li><a href="/privacy" class="hover:text-gray-300">Privacy</a></li>
-                    <li><a href="/terms-and-conditions" class="hover:text-gray-300">Terms &amp; Conditions</a></li> --}}
-
 <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800">
     <div class="mx-auto max-w-screen-xl">
         <div class="md:flex md:justify-between">
