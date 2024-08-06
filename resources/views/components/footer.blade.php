b56c3f2789604d11a58f21c5da8de43ff6d7ac3f
raza9798 <m.raza9798@gmail.com>
2024-08-07 03:03:21 +0530

    app version integrated and footer updated

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -23,8 +23,8 @@
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{now()->year}} <a href="#"
-                    class="hover:underline">{{ \App\Helpers\SiteSettingsHelper::get('name') }}™</a>. All Rights
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400" title="{{config('app.version')}}">© {{now()->year}} <a href="#"
+                    class="hover:underline">{{ \App\Helpers\SiteSettingsHelper::get('name') }}</a>. All Rights
                 Reserved.
             </span>
             <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
