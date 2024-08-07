df60f9517c3009c78c941da4a96b61ce539e47f8
raza9798 <m.raza9798@gmail.com>
2024-08-07 20:44:46 +0530

    image section removed

--- resources/views/livewire/property-detail.blade.php
+++ resources/views/livewire/property-detail.blade.php
@@ -3,9 +3,6 @@
         {{-- <article class="container mx-auto px-4 py-8">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
-            <img src="{{ $property->getFirstMediaUrl('images') ?? asset('build/images/property-placeholder.png') }}" alt="{{ $property->title }}" onerror="this.onerror=null; this.src='{{asset('build/images/property-placeholder.png')}}';" class="w-full h-auto rounded-lg shadow-lg">
-        </div>
-        <div>
             <h1 class="text-3xl font-bold mb-4">{{ $property->title }}</h1>
             <p class="text-2xl text-gray-700 mb-4">${{ number_format($property->price, 2) }}</p>
 
