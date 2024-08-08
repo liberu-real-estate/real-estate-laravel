33a4e3e4d87da87494df6799041ea61a2fc02df8
Joshua Kisb <joshuakisb@gmail.com>
2024-08-08 16:43:43 +0300

    communication hub

--- app/Filament/Staff/Resources/CommunicationHubResource/Pages/CreateCommunicationHub.php
+++ app/Filament/Staff/Resources/CommunicationHubResource/Pages/CreateCommunicationHub.php
@@ -8,4 +8,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCommunicationHub extends CreateRecord
 {
     protected static string $resource = CommunicationHubResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['sender_id'] = auth()->id();
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
