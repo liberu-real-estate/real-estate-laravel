215772e939dda0e78fccc6678d6ff55046fcbf6e
raza9798 <m.raza9798@gmail.com>
2024-08-08 20:47:30 +0530

    code review and clean

--- resources/views/livewire/property-detail.blade.php
+++ resources/views/livewire/property-detail.blade.php
@@ -200,14 +200,10 @@
                 </div>
             </section>
 
-
-            <!-- Main modal -->
             <div id="bookValuationModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
                 class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div class="relative p-4 w-full max-w-2xl max-h-full">
-                    <!-- Modal content -->
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <!-- Modal header -->
                         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                                 Book valution
@@ -223,7 +219,6 @@
                                 <span class="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <!-- Modal body -->
                         <div class="p-4 md:p-5 space-y-4">
                             @if (App\Providers\AppServiceProvider::isComponentEnabled('valuation-booking'))
                                 @livewire('valuation-booking')
@@ -238,7 +233,6 @@
                                 </div>
                             @endif
                         </div>
-                        <!-- Modal footer -->
                         <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                             <button data-modal-hide="bookValuationModal" type="button"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Book
@@ -250,14 +244,11 @@
                 </div>
             </div>
 
-            <!-- Main modal -->
             <div id="scheduleViewingModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
                 class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                 <div class="relative p-4 w-full max-w-2xl max-h-full">
-                    <!-- Modal content -->
                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                         <form wire:submit.prevent="bookViewing" class="space-y-4">
-                            <!-- Modal header -->
                             <div
                                 class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
@@ -274,7 +265,6 @@
                                     <span class="sr-only">Close modal</span>
                                 </button>
                             </div>
-                            <!-- Modal body -->
                             <div class="p-4 md:p-5 space-y-4">
                                 @if (App\Providers\AppServiceProvider::isComponentEnabled('property-booking'))
                                     @livewire('property-booking', ['propertyId' => $property->id])
@@ -289,7 +279,6 @@
                                     </div>
                                 @endif
                             </div>
-                            <!-- Modal footer -->
                             <div
                                 class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                                 <button type="submit" data-modal-hide="scheduleViewingModal" type="button"
@@ -300,7 +289,6 @@
                                     class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
                             </div>
                         </form>
-
                     </div>
                 </div>
             </div>
