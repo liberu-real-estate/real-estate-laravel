56f97c18d2f72f8e960448c12928859f87943c03
raza9798 <m.raza9798@gmail.com>
2024-08-08 20:36:12 +0530

    book valution popup modal integrated

--- resources/views/livewire/property-detail.blade.php
+++ resources/views/livewire/property-detail.blade.php
@@ -26,6 +26,7 @@
 </div>
 </article> --}}
         <section>
+
             <section class="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
                 <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
                     <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
@@ -92,7 +93,8 @@
                                     Schedule a viewing
                                 </a>
 
-                                <a href="#" title=""
+                                <a href="#" title="" data-modal-target="bookValuationModal"
+                                    data-modal-toggle="bookValuationModal"
                                     class="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
                                     role="button">
                                     <svg class="w-5 h-5 -ms-2 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -188,7 +190,8 @@
                                             to G
                                             (least efficient). Learn more about <a href="#"
                                                 class="text-blue-600 hover:underline"
-                                                @click="$dispatch('open-modal', 'energy-efficiency-info')">energy efficiency
+                                                @click="$dispatch('open-modal', 'energy-efficiency-info')">energy
+                                                efficiency
                                                 ratings</a>.</p>
                                     @else
                                         <ul class="list-disc list-inside grid grid-cols-2 gap-2">
@@ -221,5 +224,56 @@
                     @endauth
                 </div>
             </section>
+
+
+            <!-- Main modal -->
+            <div id="bookValuationModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <!-- Modal header -->
+                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                Book valution
+                            </h3>
+                            <button type="button"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                data-modal-hide="bookValuationModal">
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span class="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="p-4 md:p-5 space-y-4">
+                            @if (App\Providers\AppServiceProvider::isComponentEnabled('valuation-booking'))
+                                @livewire('valuation-booking')
+                            @endif
+
+                            @if ($isLettingsProperty)
+                                <div class="mt-8">
+                                    <a href="{{ route('tenancy.apply', $property->id) }}"
+                                        class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+                                        Apply for Tenancy
+                                    </a>
+                                </div>
+                            @endif
+                        </div>
+                        <!-- Modal footer -->
+                        <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                            <button data-modal-hide="bookValuationModal" type="button"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Book
+                                valution</button>
+                            <button data-modal-hide="bookValuationModal" type="button"
+                                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </section>
     @endsection
