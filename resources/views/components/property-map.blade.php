51ae1c6300fe355d954fb7c28d128eca07686aba
raza9798 <m.raza9798@gmail.com>
2024-09-05 22:15:28 +0530

    rendering locations in map

--- resources/views/components/property-map.blade.php
+++ resources/views/components/property-map.blade.php
@@ -1,12 +1,16 @@
 <div id="map" style="height: 400px;"></div>
 
 <script type="module">
+    let properties = @json($properties);
     var map = L.map('map').setView([51.505, -0.09], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-
     L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('test demo')
+        .bindPopup("{{ SiteConfig::get('name') }}")
         .openPopup();
+        properties.forEach(function(loc) {
+    L.marker([loc.latitude, loc.longitude]).addTo(map)
+        .bindPopup('<b>' + loc.title + '</b><br>' + loc.location);
+    });
 </script>
