2cab2cd351f2906c18567d036f5584d3cca557ac
raza9798 <m.raza9798@gmail.com>
2024-09-05 22:20:46 +0530

    map location updated

--- resources/views/components/property-map.blade.php
+++ resources/views/components/property-map.blade.php
@@ -2,7 +2,7 @@
 
 <script type="module">
     let properties = @json($properties);
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16})
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
