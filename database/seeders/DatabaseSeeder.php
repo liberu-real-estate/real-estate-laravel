9bc57f3a2189ffc098aeeb1127e233dea6a8d051
Curtis Delicata <curtis.delicata@liberu.co.uk>
2025-08-24 14:30:00 +0000

    Temporary fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SiteSettingsSeeder::class,
+//            SiteSettingsSeeder::class,
             PermissionsSeeder::class,
             RolesSeeder::class,
             DefaultTeamSeeder::class,
