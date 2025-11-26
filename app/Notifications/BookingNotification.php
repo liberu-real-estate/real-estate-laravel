d01a20d28bb507106aa018e87460a4eb76252d11
Joshua Kisb <joshuakisb@gmail.com>
2025-11-26 09:24:44 +0300

    bugfix booking notification

--- app/Notifications/BookingNotification.php
+++ app/Notifications/BookingNotification.php
@@ -1,24 +1,3 @@
-
-    {
-        return (new MailMessage)
-                    ->subject("Booking {$this->action}")
-                    ->line("Your booking for {$this->booking->property->title} has been {$this->action}.")
-                    ->line("Date: {$this->booking->date->format('F j, Y')}")
-                    ->line("Time: {$this->booking->time->format('g:i A')}");
-    }
-
-    public function toArray($notifiable)
-    {
-        return [
-            'booking_id' => $this->booking->id,
-            'action' => $this->action,
-            'property' => $this->booking->property->title,
-            'date' => $this->booking->date->format('Y-m-d'),
-            'time' => $this->booking->time->format('H:i'),
-        ];
-    }
-}
-
 <?php
 
 namespace App\Notifications;
@@ -55,4 +34,16 @@ class BookingNotification extends Notification implements ShouldQueue
                     ->line("Date: {$this->booking->date->format('F j, Y')}")
                     ->line("Time: {$this->booking->time->format('g:i A')}");
     }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'booking_id' => $this->booking->id,
+            'action' => $this->action,
+            'property' => $this->booking->property->title,
+            'date' => $this->booking->date->format('Y-m-d'),
+            'time' => $this->booking->time->format('H:i'),
+        ];
+    }
+
 }
